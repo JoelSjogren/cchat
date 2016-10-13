cd7ae17db780bc2d89244102a59ad243cd724969
Joel Sjögren <joelsjogren.wii@gmail.com>
2016-10-13 11:12:56 +0200

    server connect

--- server.erl
+++ server.erl
@@ -20,19 +20,29 @@ initial_state(ServerName) ->
 
 %% Connect to client
 handle(St, {connect, Pid, Nick}) ->
-  case lookup(Pid, Nick) of
+  case lookup(Pid, Nick, St#server_st.clients) of
     pid_exists -> {reply, {error, user_already_connected, "You are already connected."}, St};
     nick_exists -> {reply, {error, nick_taken, "Someone else is using that nickname."}, St};
     _ ->
       Model = spawn_link(fun() -> client_model(Pid, Nick) end),
       Clients = dict:store(Pid, {Nick, Model}, St#server_st.clients),
       {reply, ok, St#server_st{clients = Clients}}
-  end
-
-
+  end;
 
 handle(St, Request) ->
   io:fwrite("Server received: ~p~n", [Request]),
   Response = "hi!",
   io:fwrite("Server is sending: ~p~n", [Response]),
   {reply, Response, St}.
+
+lookup(Pid, Nick, Clients) ->
+  case dict:is_key(Pid, Clients) of
+    true -> pid_exists;
+    false ->
+      case dict:is_empty(dict:filter(fun(_Pid, {OldNick, _Model}) -> OldNick == Nick end, Clients)) of
+        true -> nick_exists;
+        false -> not_found
+      end
+  end.
+
+client_model(Pid, Nick) -> 0.
